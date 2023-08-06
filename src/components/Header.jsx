a7b265c2cd62bd1b10b3617debad5892529d991f
Ajay <ajaypanaskar8@gmail.com>
2023-08-06 12:42:04 +0530

    changes made in navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import styled from "styled-components";
 import HomeLogo from "../assets/home-logo.svg";
 import searchIcon from "../assets/search-icon.svg";
+import NavHome from "../assets/nav-home.svg";
+import MyNetwork from "../assets/nav-network.svg";
+import navJobs from "../assets/nav-jobs.svg";
+import navMessage from "../assets/nav-messaging.svg";
+import navnNotification from "../assets/nav-notifications.svg";
+import navWork from "../assets/nav-work.svg";
+import DownIcon from "../assets/down-icon.svg";
 
 const Header = (props) => {
   return (
@@ -21,6 +28,52 @@ const Header = (props) => {
             <img src={searchIcon} alt="" />
           </SearchIcon>
         </Search>
+        <Nav>
+          <NavListWrap>
+            <NavList className="active">
+              <a>
+                <img src={NavHome} alt="" />
+                <span>Home</span>
+              </a>
+            </NavList>
+
+            <NavList>
+              <a>
+                <img src={MyNetwork} alt="" />
+                <span>My Network</span>
+              </a>
+            </NavList>
+
+            <NavList>
+              <a>
+                <img src={navJobs} alt="" />
+                <span>Jobs</span>
+              </a>
+            </NavList>
+
+            <NavList>
+              <a>
+                <img src={navMessage} alt="" />
+                <span>Messaging</span>
+              </a>
+            </NavList>
+
+            <NavList>
+              <a>
+                <img src={navnNotification} alt="" />
+                <span>Notifications</span>
+              </a>
+            </NavList>
+
+            <NavList>
+              <a>
+                <img src={navWork} alt="" />
+                <span>Work</span>
+              </a>
+              <img src={DownIcon} className="downArrow" alt="" />
+            </NavList>
+          </NavListWrap>
+        </Nav>
       </Content>
     </Container>
   );
@@ -30,7 +83,7 @@ const Container = styled.div`
   background-color: white;
   border-bottom: 1px solid rgba(0, 0, 0, 0.08);
   left: 0;
-  padding: 0 24px;
+  padding: 5px 24px;
   position: fixed;
   top: 0;
   width: 100vw;
@@ -43,6 +96,7 @@ const Content = styled.div`
   margin: 0 auto;
   min-height: 100%;
   max-width: 1128px;
+  /* justify-content: space-between; */
 `;
 
 const Logo = styled.a``;
@@ -82,6 +136,78 @@ const SearchIcon = styled.div`
   justify-content: center;
   transition: backgroun-color 0.15s;
   align-items: center;
+`;
+
+const Nav = styled.nav`
+  margin-left: auto;
+  display: block;
+  @media (max-width: 768px) {
+    /* display: none; */
+    /* padding: 0 0; */
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    background: white;
+    width: 100%;
+  }
+`;
+
+const NavListWrap = styled.ul`
+  display: flex;
+  flex-wrap: nowrap;
+  list-style: none;
+
+  .active {
+    border-bottom: 4px solid black;
+    /* span:after {
+      content: "";
+      transform: scaleX(1);
+      border-bottom: 2px solid var(--white, #fff);
+      bottom: 0;
+      left: 0;
+      position: absolute;
+      transition: transform 0.2s;
+    } */
+  }
+`;
+const NavList = styled.li`
+  display: flex;
+  align-items: center;
+
+  .downArrow {
+    margin-left: -1.5rem;
+    margin-bottom: 1rem;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    background: transparent;
+    flex-direction: column;
+    font-size: 12px;
+    font-weight: 400;
+    justify-content: center;
+    line-height: 1.5;
+    min-height: 42px;
+    min-width: 80px;
+    position: relative;
+    text-decoration: none;
+    cursor: pointer;
+
+    span {
+      color: rgba(0, 0, 0, 0.6);
+      display: flex;
+      align-items: center;
+    }
+
+    @media (max-width: 768px) {
+      min-width: 70px;
+    }
+
+    span:hover {
+      color: rgba(0, 0, 0, 0.9);
+    }
+  }
 `;
 
 export default Header;
